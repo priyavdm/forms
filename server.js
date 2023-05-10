4dd0e7247ffcc1a83951e55052c1f28b172299aa
priyavdm <priyavdmtpk@gmail.com>
2023-05-10 19:16:09 +0530

    add my form

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'Password123',
+    database : 'forms'
+  });
+
+var cors =require('cors') 
+   
+app=express()
+app.use(express.json())
+app.use(cors())
+connection.connect();
+
+app.get('/getall',(req,res)=>{
+    connection.query('SELECT * from users', function (error, results) {
+        if (error){
+            console.log(error);
+        }
+        console.log('The solution is: ', results);
+        res.json(results)
+    
+      });
+    });
+
+app.post('/insert',(req,res)=>{
+        // console.log(req.body,"gfgtt")
+        connection.query(`insert into users (username,email,subjects,message) values ('${req.body.username}','${req.body.email}','${req.body.subjects}','${req.body.message}')`, function (error, results) {
+         if (error) {
+        console.log(error);
+    }
+        
+         res.json(results)
+     });
+    });
+
+app.put('/update',(req,res)=>{
+    
+       connection.query('update users set username=?,email=?,subjects=?,message=? where id=?',[req.body.username,req.body.email,req.body.subjects,req.body.message,req.body.id],function (error, results) {
+ if (error) {
+                console.log(error);       
+        }        
+        res.json(results)
+        });
+        
+        })
+
+app.listen(3000,()=>{
+   console.log("Listening on port 3000");
+ })  
